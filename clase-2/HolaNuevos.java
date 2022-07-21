5e8e5db0078bac114c8bd420f06659ea6d2d6c73
Erick Alvarez <becker7000@outlook.es>
2022-07-21 11:16:59 -0500

    variables

---
+++ clase-2/HolaNuevos.java
@@ -0,0 +1,11 @@
+public class HolaNuevos {
+
+    // Atajo: escribir "main" para generar la cabecera del método main
+    public static void main(String[] args) {
+
+        // Atajo: escribir "sout" para generar la línea de abajo:
+        System.out.println("\n\t Hola a todos de nuevo... ");
+
+    }
+
+}
